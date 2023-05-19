70a8a39e244d2a061e8f76611c8196a2a078c515
nurzzaat <210103336@stu.sdu.edu.kz>
2023-05-19 22:57:14 +0600

    places of mines

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import tkinter as tr
-
+from random import shuffle
 
 class Saper:
     window = tr.Tk()
     row = 10
     col = 10
+    mine = 10
     buttons = []
 
     def __init__(self):
+        self.buttons = []
+        count = 1
         for i in range(Saper.row):
             temp = []
             for j in range(Saper.col):
-                button = Button(Saper.window, x=i, y=j, width=3, font='Arial 13 bold')
+                button = Saper_Button(Saper.window, x=i, y=j, number=count, width=3, font='Arial 13 bold')
                 temp.append(button)
-            Saper.buttons.append(temp)
+                count += 1
+            self.buttons.append(temp)
 
     def start(self):
         self.create_btn()
+        self.insert_mine()
         Saper.window.mainloop()
 
     def create_btn(self):
@@ -25,13 +30,31 @@ class Saper:
                 btn = self.buttons[i][j]
                 btn.grid(row=i, column=j)
 
+    @staticmethod
+    def place_of_mines():
+        index_list = list(range(1, Saper.row * Saper.col + 1))
+        print(index_list)
+        shuffle(index_list)
+        return index_list[:Saper.mine]
 
-class Button(tr.Button):
-    def __init__(self, master, x, y, number=0, *args, **kwargs):
-        super(Button, self).__init__(master, *args, **kwargs)
+    def insert_mine(self):
+        index_mine = self.place_of_mines()
+        for btns in self.buttons:
+            for btn in btns:
+                if btn.number in index_mine:
+                    print(btn.number)
+                    btn.is_mine = True
+
+
+class Saper_Button(tr.Button):
+
+    def __init__(self, master, x, y, number, *args, **kwargs):
+        super(Saper_Button, self).__init__(master, *args, **kwargs)
         self.x = x
         self.y = y
         self.number = number
+        self.is_mine = False
+
 
 game = Saper()
 game.start()
